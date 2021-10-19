144d73baecd2f34c2de112f3b4158f1e94e56d7a
nghia45 <godresiv@gmail.com>
2021-10-19 23:14:46 +0700

    Final version

--- README.md
+++
@@ -1,7 +0,0 @@
-# Bài tập lớn Từ điển
-
-- Môn học: OOP
-- Ngôn ngữ: Java
-- Thư viện giao diện đồ hoạ:
-
-## Các chức năng chính: 
--- src/Dictionary/Dictionary.java
+++
@@ -1,8 +0,0 @@
-package Dictionary;
-
-import java.util.ArrayList;
-
-public class Dictionary {
-    //Contain a list of words and meanings
-    public static ArrayList<Word> list = new ArrayList<>();
-}
